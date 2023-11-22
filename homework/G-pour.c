54ba775734e47ee48e4f99a0f04671aae815fbbb
Re_Char <3372488357@qq.com>
2023-11-22 11:11:39 +0800

    CPL

--- homework/G-pour.c
+++ homework/G-pour.c
@@ -15,27 +15,27 @@ bool pour(int a, int b, int c, int a0, int b0, int c0, int k, int VA, int VB, in
         return 0;
     }
     int pourAtoB = (a < VB - b) ? a : (VB - b);
-    if (pourAtoB > 0 && pour(a - pourAtoB, b + pourAtoB, c, a0, b0, c0, k - 1, VA, VB, VC)) {
+    if (pourAtoB > 0 && pour(0, b + pourAtoB, c, a0, b0, c0, k - 1, VA, VB, VC)) {
         return 1;
     }
     int pourAtoC = (a < VC - c) ? a : (VC - c);
-    if (pourAtoC > 0 && pour(a - pourAtoC, b, c + pourAtoC, a0, b0, c0, k - 1, VA, VB, VC)) {
+    if (pourAtoC > 0 && pour(0, b, c + pourAtoC, a0, b0, c0, k - 1, VA, VB, VC)) {
         return 1;
     }
     int pourBtoA = (b < VA - a) ? b : (VA - a);
-    if (pourBtoA > 0 && pour(a + pourBtoA, b - pourBtoA, c, a0, b0, c0, k - 1, VA, VB, VC)) {
+    if (pourBtoA > 0 && pour(a + pourBtoA, 0, c, a0, b0, c0, k - 1, VA, VB, VC)) {
         return 1;
     }
     int pourBtoC = (b < VC - c) ? b : (VC - c);
-    if (pourBtoC > 0 && pour(a, b - pourBtoC, c + pourBtoC, a0, b0, c0, k - 1, VA, VB, VC)) {
+    if (pourBtoC > 0 && pour(a, 0, c + pourBtoC, a0, b0, c0, k - 1, VA, VB, VC)) {
         return 1;
     }
     int pourCtoA = (c < VA - a) ? c : (VA - a);
-    if (pourCtoA > 0 && pour(a + pourCtoA, b, c - pourCtoA, a0, b0, c0, k - 1, VA, VB, VC)) {
+    if (pourCtoA > 0 && pour(a + pourCtoA, b, 0, a0, b0, c0, k - 1, VA, VB, VC)) {
         return 1;
     }
     int pourCtoB = (c < VB - b) ? c : (VB - b);
-    if (pourCtoB > 0 && pour(a, b + pourCtoB, c - pourCtoB, a0, b0, c0, k - 1, VA, VB, VC)) {
+    if (pourCtoB > 0 && pour(a, b + pourCtoB, 0, a0, b0, c0, k - 1, VA, VB, VC)) {
         return 1;
     }
     if (a > 0 && b > 0) {
